a0d820333a11620e36ca2b9c2df971524f5e8904
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-14 08:43:50 +0530

    added code to create a table

--- sql.sql
+++ sql.sql
@@ -1,18 +1,21 @@
 -- Create a DATABASE
-CREATE DATABASE name;
+CREATE DATABASE db;
 
 -- delete a database
-DROP DATABASE name;
+DROP DATABASE db;
 
 -- Alterning to read only mode
 -- Cannot drop database in read only mode
-ALTER DATABASE name READ ONLY = 1;
+ALTER DATABASE db READ ONLY = 1;
 
 -- use the database
-USE name;
-
-
+USE db;
 
 -- creating a table
+CREATE TABLE products(
+    id int,
+    name varchar(20),
+    spoiled date
+)
 
 
