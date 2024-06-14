8294c219ece09ca26abb313556d48fc8855480f6
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-14 10:07:58 +0530

    populated all row and select commands

--- row.sql
+++ row.sql
@@ -1,8 +1,47 @@
 -- insert row
+INSERT INTO products
+-- values must be in order as the schema describes
+VALUES(
+    1,
+    100,
+    "jonsnow",
+    "blackWatch",
+    "2023-04-01"
+)
+
 
 -- insert multiple rows
+INSERT INTO products
+-- values must be in order as the schema describes
+VALUES
+    (
+        1,
+        100,
+        "jonsnow",
+        "blackWatch",
+        "2023-04-01"
+    ),
+    (
+        2,
+        200,
+        "bran",
+        "stark",
+        "2024-03-02"
+    );
+
 
 -- insert data as new row into specific columns
 -- other data fields will be NULL
+INSERT INTO products
+VALUES
+    (
+        7,
+        100,
+        "tyrian",
+        "lannistar",
+        "2023-02-01"
+    );
 
--- Add data into existing row
+-- Fill up the NULL values using ADD
+
+-- Modify row
--- select.sql
+++ select.sql
@@ -1,6 +1,12 @@
+-- It will display NULL values if NULL and empty if doesn't exit
+
 -- query all columns
 SELECT * from products;
 
 -- query specific column
+SELECT id, fname from products;
+
 
 -- where clause to get specific rows
+SELECT * FROM products
+WHERE id = 1
