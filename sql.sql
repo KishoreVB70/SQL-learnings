66a395064ed45e61f21cf8ca90142d17185072f9
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-14 08:40:39 +0530

    added code to alter and use database

--- sql.sql
+++ sql.sql
@@ -3,3 +3,16 @@ CREATE DATABASE name;
 
 -- delete a database
 DROP DATABASE name;
+
+-- Alterning to read only mode
+-- Cannot drop database in read only mode
+ALTER DATABASE name READ ONLY = 1;
+
+-- use the database
+USE name;
+
+
+
+-- creating a table
+
+
