d582feec1ea694de8df28699aded96b3c05fc2de
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-19 09:25:47 +0530

    Added subquery

--- select.sql
+++ select.sql
@@ -96,3 +96,15 @@ union
 select id, city  from city
 where power > 50
 order by fname
+
+-- Sub query
+select fname, house, kingdom, power, 
+(select avg(power) from chc) as Avg_power 
+from chc
+where power > (select avg(power) from chc);
+
+-- Long useful version spanning 2 tables
+select ruler from city
+where id in
+(select city_id from chc
+where power > (select avg(power) from chc))
